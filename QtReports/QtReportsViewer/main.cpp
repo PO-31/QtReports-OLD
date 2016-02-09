e733628ddfd5c13acce5e219a8f1e472771356a4
Константин Стуков <havendv@hotmail.com>
2016-02-09 16:30:20 +1000

    Added template for QtReportViewer.

--- QtReports/QtReportsViewer/main.cpp
+++ QtReports/QtReportsViewer/main.cpp
@@ -1,10 +1,13 @@
-#include "qtreportsviewer.h"
 #include <QtWidgets/QApplication>
 
-int main(int argc, char *argv[])
-{
-	QApplication a(argc, argv);
-	QtReportsViewer w;
-	w.show();
+int main( int argc, char *argv[] ) {
+	QApplication a( argc, argv );
+
+	qtreports::Engine engine;
+	//QString( argv[ 1 ] ) or std::string( argv[ 1 ] )
+	engine.compile( "blabla.report" );
+	qtreports::WidgetPtr widget = engine.getWidget();
+	widget->show();
+
 	return a.exec();
 }
--- QtReports/QtReportsViewer/qtreportsviewer.pro
+++ QtReports/QtReportsViewer/qtreportsviewer.pro
@@ -1,9 +1,4 @@
 TARGET = QtReportsViewer
 QT += core widgets gui
 CONFIG += release
-DEFINES += QT_DLL QT_WIDGETS_LIB
-HEADERS += ./qtreportsviewer.h
-SOURCES += ./main.cpp \
-    ./qtreportsviewer.cpp
-FORMS += ./qtreportsviewer.ui
-RESOURCES += qtreportsviewer.qrc
+SOURCES += ./main.cpp
