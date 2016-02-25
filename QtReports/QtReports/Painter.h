233153c68acab015acf80de334dfe4c818f50d8d
Vladimir <syler1983.9@gmail.com>
2016-02-25 15:04:04 +0900

    Закончен интерфейс Style и прокоментирован Painter

--- QtReports/QtReports/Painter.h
+++ QtReports/QtReports/Painter.h
@@ -3,6 +3,8 @@
 
 #include <QString>
 
+#include "Style.h"
+
 /* Осуществляет отображение отчета в некоторые форматы. */
 
 class Painter
@@ -10,14 +12,22 @@ class Painter
 public:
     Painter();
 
-    bool beginPDF_Drawing(QString path); // Вызывается первой
+    bool beginPDF_Drawing(QString path); /* Эта функция задает формат и файл, в которые будет транслирован готовый отчет.
+                                           Вызывается перед началом рисования один раз. */
 
-    int beginObjectDrawing(); // Вызывается перед отрисовкой конкретного объекта, возвращает его код
+    int beginObjectDrawing(); /* Вызывается перед отрисовкой конкретного объекта, возвращает его код. Теперь все последующие
+                                действия (задание рабочей зоны, рисование объектов, задание стиля и пр.) будут ассойиированы
+                                с этим объектом. */
     bool setWorkspace(int x, int y, int w, int h); /* Устанавливает зону отрисовки для последнего объекта,
-                                                      вызвавшего begin_object_drawing(). */
-    bool endObjectDrawing(int workspace_id);
+                                                      вызвавшего begin_object_drawing().
+                                                      Причем координаты x и y задаются относительно координат области объекта,
+                                                      вызвавшего setWorkspace выше. Т.е. рабочие области вкладываются.
+                                                      Кроме того, рабочая область потомка не может выходить за пределы рабочей
+                                                      области родителя. */
 
-    void pageDrawingComplete(); // Завершает отрисовку данной страницы, используется объектом Report
+    bool endObjectDrawing(int workspace_id); /* Заканчивает рисовать объект. */
+
+    void pageDrawingComplete(); /* Завершает отрисовку данной страницы, используется объектом Report. */
 
     bool drawLine(int x1, int y1, int x2, int y2);
 
@@ -35,11 +45,15 @@ public:
 
     int currPage();
 
+    void setStyle(int style_id);
+
     QString lastError();
 
 protected:
 
     void newPage(); // Создать новую страницу
+
+    void setStyle(const Style& style); // Задать стиль отрисовки
 };
 
 #endif // PAINTER_H
--- QtReports/QtReports/Style.h
+++ QtReports/QtReports/Style.h
@@ -2,6 +2,9 @@
 #define STYLE_H
 
 
+#include <QString>
+#include <QColor>
+
 class Style
 {
 public:
@@ -9,6 +12,29 @@ public:
 
     int id() const;
     void setId(int _id);
+
+    QString getName() const;
+    void setName(QString name);
+
+    bool isDefault() const; // Стиль по умолчанию
+    void setDefault(bool def);
+
+    int fontSize() const; // Размер шрифта
+    void setFontSize(int size);
+
+    QColor fontColor() const; // Цвет шрифта
+    void setFontColor(QColor f_color);
+
+    QString fontName() const; // Имя шрифта
+    void setFontName(QString f_name);
+
+    bool isBold() const; // Жирный шрифт
+    void setBold(bool flag);
+
+    bool isItalic() const; // Курсив
+    void setItalic(bool flag);
+
+protected:
 };
 
 #endif // STYLE_H
