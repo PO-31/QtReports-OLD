27240bb221ea7134c6d20f731a28d45e027f56ab
Vladimir <syler1983.9@gmail.com>
2016-02-28 11:55:03 +0900

    Небольшие изменения в функции draw

---
+++ report.cpp
@@ -0,0 +1,83 @@
+#include "report.hpp"
+
+namespace qtreports {
+	namespace detail {
+
+		Report::Report() :
+			m_isVertical( true ),
+			m_size( 600, 400 ) {}
+
+		Report::~Report() {}
+
+		bool	Report::draw( Painter & painter, const ProcessedDB & db ) {
+            bool new_page_req = false;
+            do {
+				if( !Object::draw( painter, db ) ) {
+					return false;
+				}
+
+                new_page_req = painter.isNewPageRequested();
+				painter.pageDrawingComplete();
+			} 
+            while( new_page_req );
+
+			return true;
+		}
+
+		QString		Report::asHTML() const {
+			return "Hello, world";
+		}
+
+		bool Report::setParameter( const QString & name, const QVariant & value ) {
+			QString page_width( "page_width" ), page_height( "page_height" ),
+				isVertical( "is_vertical" );
+
+			if( !value.isValid() ) {
+				m_lastError = "Invalid parameter value";
+				return false;
+			}
+
+			if( page_width.compare( name, Qt::CaseInsensitive ) == 0 ) {
+				if( value.type() != QVariant::Int ) {
+					m_lastError = "Invalid type for this argument";
+					return false;
+				}
+
+				m_size.setWidth( value.toInt() );
+				return true;
+			}
+
+			if( page_height.compare( name, Qt::CaseInsensitive ) == 0 ) {
+				if( value.type() != QVariant::Int ) {
+					m_lastError = "Invalid type for this argument";
+					return false;
+				}
+
+				m_size.setWidth( value.toInt() );
+
+				return true;
+			}
+
+			if( isVertical.compare( name, Qt::CaseInsensitive ) == 0 ) {
+				if( value.type() != QVariant::Int ) {
+					m_lastError = "Invalid type for this argument";
+					return false;
+				}
+
+				m_isVertical = value.toBool();
+				return true;
+			}
+
+			return Object::setParameter( name, value );
+		}
+
+		bool	Report::drawSelf( Painter & /*painter*/, const ProcessedDB & /*db*/ ) {
+			return false;
+		}
+
+		bool	Report::prepareChilds( Painter & /*painter*/, const ProcessedDB & /*db*/ ) {
+			return false;
+		}
+
+	}
+}
