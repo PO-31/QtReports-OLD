1dcf4ecb81290cf1b19f0679dba3a14b70e4b0ae
Константин Стуков <havendv@hotmail.com>
2016-03-02 02:09:32 +1000

    Fixed engine.hpp

--- QtReports/QtReports/engine.hpp
+++ QtReports/QtReports/engine.hpp
@@ -18,32 +18,30 @@ namespace qtreports {
     public:
         Engine( QObject * parent = 0 );
         ~Engine();
-
-        bool	compile( const QString & path );
-        bool	setParameters( const QMap< QString, QString > & map );
-        bool	setConnection( const QSqlDatabase & connection );
-
+        
+        bool    compile( const QString & path );
+        bool    setParameters( const QMap< QString, QString > & map );
+        bool    setConnection( const QSqlDatabase & connection );
+        
         void    addQuery( const QString & query );
         void    addScript( const QString & script );
 
-        bool	createPDF( const QString & path );
-        bool	createHTML( const QString & path );//etc.
-        bool	print();
-
-        const QString		getLastError() const;
-        const QWidgetPtr	getWidget() const;
-        bool				isCompiled() const;
+        bool    createPDF( const QString & path );
+        bool    createHTML( const QString & path );//etc.
+        bool    print();
+        
+        const QString       getLastError() const;
+        const QWidgetPtr    getWidget() const;
+        bool                isCompiled() const;
 
 
     private:
-        QString		m_lastError, m_compiledPath;
-        QWidgetPtr	m_widget;
-        bool		m_isCompiled;
-
-        QList<QString> m_dbQueriesList;
-        QList<QString> m_scriptsList;
-
-        QSqlDatabase m_dbConnection;
+        bool		        m_isCompiled;
+        QString		        m_lastError, m_compiledPath;
+        QWidgetPtr	        m_widget;
+        QList< QString >    m_dbQueriesList;
+        QList< QString >    m_scriptsList;
+        QSqlDatabase        m_dbConnection;
 
         void		drawPreview( QPrinter * printer );
         void        prepareDB();
