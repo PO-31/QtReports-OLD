880c6ec31befcd4291fa0178e63c097bc715e3cb
Константин Стуков <havendv@hotmail.com>
2016-03-03 19:38:25 +1000

    Replaced to QGuiApplication. Need X Server error fix.

--- tests/qtreportslib_tests/main.cpp
+++ tests/qtreportslib_tests/main.cpp
@@ -1,9 +1,9 @@
-#include <QApplication>
+#include <QGuiApplication>
 #include <QTest>
 #include "test_engine.hpp"
 
 int main( int argc, char *argv[] ) {
-    QApplication a( argc, argv );
+    QGuiApplication a( argc, argv );
     QTest::qExec( new Test_Engine, argc, argv );
 
     return a.exec();
