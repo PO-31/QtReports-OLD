0baccec10148424dccbd1f8a8cd68e5595874147
Константин Стуков <havendv@hotmail.com>
2016-03-03 19:21:15 +1000

    Replace QCoreApplication to QAppliation for QWidget work.

--- tests/qtreportslib_tests/main.cpp
+++ tests/qtreportslib_tests/main.cpp
@@ -1,9 +1,9 @@
-#include <QCoreApplication>
+#include <QApplication>
 #include <QTest>
 #include "test_engine.hpp"
 
-int main(int argc, char *argv[]) {
-    QCoreApplication a(argc, argv);
+int main( int argc, char *argv[] ) {
+    QApplication a( argc, argv );
     QTest::qExec( new Test_Engine, argc, argv );
 
     return a.exec();
