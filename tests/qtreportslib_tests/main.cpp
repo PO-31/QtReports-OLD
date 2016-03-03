73c2e433fd7f7a5dd2b75eb62dc04458d4043aeb
Константин Стуков <havendv@hotmail.com>
2016-03-03 19:54:56 +1000

    Added Virtual X Server.

--- tests/qtreportslib_tests/main.cpp
+++ tests/qtreportslib_tests/main.cpp
@@ -3,7 +3,7 @@
 #include "test_engine.hpp"
 
 int main( int argc, char *argv[] ) {
-    QGuiApplication a( argc, argv );
+    QApplication a( argc, argv );
     QTest::qExec( new Test_Engine, argc, argv );
 
     return a.exec();
