eca14c4eced702ec02bdf7b4859585a4b98a2491
Константин Стуков <havendv@hotmail.com>
2016-03-03 15:16:47 +1000

    Added install scripts(For set env)

--- .travis/linux/clang/before_install.sh
+++ .travis/linux/clang/before_install.sh
@@ -5,6 +5,4 @@ sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo apt-add-repository --yes ppa:beineri/opt-qt551
 sudo apt-get update -qq
 sudo apt-get -f install clang qt-latest
-pip install --user cpp-coveralls
-source /opt/qt55/bin/qt55-env.sh
-export USING_QT_MKSPEC="linux-clang"
+sudo pip install --user cpp-coveralls
--- .travis/linux/gcc/before_install.sh
+++ .travis/linux/gcc/before_install.sh
@@ -4,5 +4,4 @@ sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
 sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
 sudo apt-get install gcc-4.8 g++-4.8 qt5-default
-pip install --user cpp-coveralls
-export USING_QT_MKSPEC="linux-g++-64"
+sudo pip install --user cpp-coveralls
