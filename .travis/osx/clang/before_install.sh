7e36344e1c69e17ea8bd64ffd91c3768ef43ee4e
Константин Стуков <havendv@hotmail.com>
2016-03-03 15:21:15 +1000

    Added osx template.

---
+++ .travis/osx/clang/before_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
+sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
+sudo apt-add-repository --yes ppa:beineri/opt-qt551
+sudo apt-get update -qq
+sudo apt-get -f install clang qt-latest
+sudo pip install --user cpp-coveralls
---
+++ .travis/osx/clang/install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source /opt/qt55/bin/qt55-env.sh
+export USING_QT_MKSPEC="linux-clang"
