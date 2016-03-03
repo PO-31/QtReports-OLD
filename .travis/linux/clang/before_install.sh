5cdac6d907572af37a97a997c13e6e010e0e76c6
Константин Стуков <havendv@hotmail.com>
2016-03-03 15:27:47 +1000

    Updates pip use.

--- .travis/linux/clang/before_install.sh
+++ .travis/linux/clang/before_install.sh
@@ -5,4 +5,5 @@ sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo apt-add-repository --yes ppa:beineri/opt-qt551
 sudo apt-get update -qq
 sudo apt-get -f install clang qt-latest
-sudo pip install --user cpp-coveralls
+sudo -H pip install --upgrade pip
+sudo -H pip install --user cpp-coveralls
--- .travis/linux/gcc/before_install.sh
+++ .travis/linux/gcc/before_install.sh
@@ -4,4 +4,5 @@ sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
 sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
 sudo apt-get install gcc-4.8 g++-4.8 qt5-default
-sudo pip install --user cpp-coveralls
+sudo -H pip install --upgrade pip
+sudo -H pip install --user cpp-coveralls
