e7f9d846e7cd1ee4905f5807880b6ca31b200b6e
Константин Стуков <havendv@hotmail.com>
2016-03-06 23:27:04 +1000

    Added ruby

--- .travis/linux/before_install.sh
+++ .travis/linux/before_install.sh
@@ -7,5 +7,5 @@ if [ "$BUILD_TYPE" = "coverage" ]; then sudo -H pip install --upgrade pip; fi
 if [ "$BUILD_TYPE" = "coverage" ]; then sudo -H pip install --user cpp-coveralls; fi
 if [ "$BUILD_TYPE" = "coverage" ]; then sudo pip install --upgrade pip; fi
 if [ "$BUILD_TYPE" = "coverage" ]; then sudo pip install --user cpp-coveralls; fi
-if [ "$BUILD_TYPE" = "coverage" ]; then sudo apt-get install -y rubygems lcov; fi
+if [ "$BUILD_TYPE" = "coverage" ]; then sudo apt-get install -y ruby-full rubygems lcov; fi
 if [ "$BUILD_TYPE" = "coverage" ]; then gem install coveralls-lcov; fi
