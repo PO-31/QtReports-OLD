db63a2842ff697c39c35f7254e5b795f09838d0c
Константин Стуков <havendv@hotmail.com>
2016-03-07 00:00:15 +1000

    Test

--- .travis/linux/before_install.sh
+++ .travis/linux/before_install.sh
@@ -3,9 +3,11 @@
 sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
 sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
-if [ "$BUILD_TYPE" = "coverage" ]; then sudo -H pip install --upgrade pip; fi
-if [ "$BUILD_TYPE" = "coverage" ]; then sudo -H pip install --user cpp-coveralls; fi
-if [ "$BUILD_TYPE" = "coverage" ]; then sudo pip install --upgrade pip; fi
-if [ "$BUILD_TYPE" = "coverage" ]; then sudo pip install --user cpp-coveralls; fi
-if [ "$BUILD_TYPE" = "coverage" ]; then sudo apt-get install -y ruby-full rubygems lcov; fi
-if [ "$BUILD_TYPE" = "coverage" ]; then gem install coveralls-lcov; fi
+if [ "$BUILD_TYPE" = "coverage" ]; then 
+  #sudo -H pip install --upgrade pip
+  #sudo -H pip install --user cpp-coveralls
+  sudo pip install --upgrade pip
+  sudo pip install --user cpp-coveralls
+  sudo apt-get install -y ruby-full rubygems lcov
+  gem install lcoveralls
+fi
