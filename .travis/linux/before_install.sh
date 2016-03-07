9b709df2774b281eba780884fc44301be31ceec7
Константин Стуков <havendv@hotmail.com>
2016-03-07 17:31:46 +1000

    Test coverage: add qtbase5-dev

--- .travis/linux/before_install.sh
+++ .travis/linux/before_install.sh
@@ -6,8 +6,8 @@ sudo apt-get update -qq
 if [ "$BUILD_TYPE" = "coverage" ]; then 
   #sudo -H pip install --upgrade pip
   #sudo -H pip install --user cpp-coveralls
-  sudo pip install --upgrade pip
-  sudo pip install --user cpp-coveralls
+  #sudo pip install --upgrade pip
+  #sudo pip install --user cpp-coveralls
   sudo apt-get install -y ruby-full rubygems lcov gdb
   gem install lcoveralls
 fi
