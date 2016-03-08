5e7de04f61655f2b40170d218ba5bf0db7374a60
Константин Стуков <havendv@hotmail.com>
2016-03-08 19:08:02 +1000

    Test coverage

--- .travis/linux/before_install.sh
+++ .travis/linux/before_install.sh
@@ -8,7 +8,7 @@ if [ "$BUILD_TYPE" = "coverage" ]; then
   #sudo -H pip install --user cpp-coveralls
   #sudo pip install --upgrade pip
   #sudo pip install --user cpp-coveralls
-  sudo apt-get install -y ruby1.9.1 rubygems1.9.1 lcov gdb
+  sudo apt-get install -y ruby1.9.1 rubygems1.9.1
   gem install lcoveralls
   sudo update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby1.9.1 100
   sudo update-alternatives --install /usr/bin/gem gem /usr/bin/gem1.9.1 100
