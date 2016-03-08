53267882d0f2811a92c43dc3cae5deaf03c8f623
Константин Стуков <havendv@hotmail.com>
2016-03-08 15:23:12 +1000

    Fix

--- .travis/linux/before_install.sh
+++ .travis/linux/before_install.sh
@@ -8,6 +8,6 @@ if [ "$BUILD_TYPE" = "coverage" ]; then
   #sudo -H pip install --user cpp-coveralls
   #sudo pip install --upgrade pip
   #sudo pip install --user cpp-coveralls
-  sudo apt-get install -y ruby-full rubygems lcov-4.8 gdb
+  sudo apt-get install -y ruby-full rubygems lcov gdb
   gem install lcoveralls
 fi
