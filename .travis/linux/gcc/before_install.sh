18e56e091b31f7ac4f8c65814e046d8792ab49fc
Константин Стуков <havendv@hotmail.com>
2016-03-08 14:07:28 +1000

    Test coverage

--- .travis/linux/gcc/before_install.sh
+++ .travis/linux/gcc/before_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt-get install gcc-4.8 g++-4.8 gcov-4.8 qt5-default qttools5-dev-tools
+sudo apt-get install gcc-4.8 g++-4.8 qt5-default qttools5-dev-tools
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 90
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 90
 sudo update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-4.8 90
