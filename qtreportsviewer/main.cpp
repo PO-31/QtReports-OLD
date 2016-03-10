871744bd1f6394bb485fe5f9a5c81d0e7c5a50ac
Константин Стуков <havendv@gmail.com>
2016-03-10 17:36:27 +1000

    Added files for previosly commit.

--- qtreportsviewer/main.cpp
+++ qtreportsviewer/main.cpp
@@ -16,7 +16,7 @@ int main( int argc, char *argv[] ) {
     QMenu file( "File", &bar );
     QMenu convert( "Convert", &bar );
 
-    QString path = argc > 1 ? argv[ 1 ] : "../tests/qtreportslib_tests/simple.qreport";
+    QString path = argc > 1 ? argv[ 1 ] : "../tests/qtreportslib_tests/detail.qreport";
     qtreports::Engine engine( path );
     if( !engine.isOpened() ) {
         QMessageBox::critical( 0, "Error: ", engine.getLastError() );
