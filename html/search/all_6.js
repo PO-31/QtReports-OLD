ec40e63f1589e486d2c3ac23d4a214274d243d96
travic <travic@travic.org>
2016-03-28 00:58:59 +0000

    Update doc from Travic CI

--- html/search/all_6.js
+++ html/search/all_6.js
@@ -1,7 +1,9 @@
 var searchData=
 [
+  ['getcolumn',['getColumn',['../classqtreports_1_1detail_1_1_processed_d_b.html#aa605f08d627d075b24ec5940e874b82b',1,'qtreports::detail::ProcessedDB::getColumn(const QString &amp;name, QVector&lt; QVariant &gt; &amp;result)'],['../classqtreports_1_1detail_1_1_processed_d_b.html#abd9759818a49ea6400137b0e67823917',1,'qtreports::detail::ProcessedDB::getColumn(int col, QVector&lt; QVariant &gt; &amp;result)']]],
   ['geterror',['getError',['../classqtreports_1_1detail_1_1_processed_d_b.html#a6d1e67b6a92c145f39752dfc43d42a3b',1,'qtreports::detail::ProcessedDB']]],
-  ['getfield',['getField',['../classqtreports_1_1detail_1_1_processed_d_b.html#adf6f9603d376241683343ca4001c5ae0',1,'qtreports::detail::ProcessedDB::getField(const QString &amp;queryName, const QString &amp;columnName, int row, QVariant &amp;result)'],['../classqtreports_1_1detail_1_1_processed_d_b.html#ac7e8d8d44c5b60f950b41799505db471',1,'qtreports::detail::ProcessedDB::getField(const QString &amp;queryName, int column, int row, QVariant &amp;result)']]],
+  ['getfield',['getField',['../classqtreports_1_1detail_1_1_processed_d_b.html#a00cd37e89fee429edddec7bc16c1d9b0',1,'qtreports::detail::ProcessedDB::getField(const QString &amp;columnName, int row, QVariant &amp;result)'],['../classqtreports_1_1detail_1_1_processed_d_b.html#adee6c635266f257582b0441a4cd34976',1,'qtreports::detail::ProcessedDB::getField(int column, int row, QVariant &amp;result)']]],
+  ['getfieldindex',['getFieldIndex',['../classqtreports_1_1detail_1_1_processed_d_b.html#a6b9729908a55c7c3ff35589e914b0519',1,'qtreports::detail::ProcessedDB']]],
   ['getlasterror',['getLastError',['../classqtreports_1_1detail_1_1_parser.html#acb2cceb86384551fed0652b0bfcd9383',1,'qtreports::detail::Parser']]],
   ['getlog',['getLog',['../classqtreports_1_1detail_1_1_parser.html#ace6817f32afed3501061adb7a8035f45',1,'qtreports::detail::Parser']]],
   ['getparam',['getParam',['../classqtreports_1_1detail_1_1_processed_d_b.html#abf442da7e6e788a861bdddb278cf1d7b',1,'qtreports::detail::ProcessedDB']]],
--- html/search/functions_0.js
+++ html/search/functions_0.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['addexecutedquery',['addExecutedQuery',['../classqtreports_1_1detail_1_1_processed_d_b.html#ad4b4304720814ec08736ef078afdad89',1,'qtreports::detail::ProcessedDB']]],
+  ['addcolumn',['addColumn',['../classqtreports_1_1detail_1_1_processed_d_b.html#adde605948b357a49afff2e5d47e64383',1,'qtreports::detail::ProcessedDB']]],
+  ['addfielddata',['addFieldData',['../classqtreports_1_1detail_1_1_processed_d_b.html#a71c710be24a7aabfa7f36c9565274bd0',1,'qtreports::detail::ProcessedDB::addFieldData(const QString &amp;columnName, const QVariant &amp;data)'],['../classqtreports_1_1detail_1_1_processed_d_b.html#ab0b66ff6cd918784ecb963d254bad7e8',1,'qtreports::detail::ProcessedDB::addFieldData(const QString &amp;columnName, const QVector&lt; QVariant &gt; &amp;data)'],['../classqtreports_1_1detail_1_1_processed_d_b.html#a0fb14688c44b7b16c67c79cd7fe850f5',1,'qtreports::detail::ProcessedDB::addFieldData(int column, const QVariant &amp;data)']]],
   ['addparam',['addParam',['../classqtreports_1_1detail_1_1_processed_d_b.html#ad35bba9804025d0b794b6c136ee5b8ae',1,'qtreports::detail::ProcessedDB']]]
 ];
