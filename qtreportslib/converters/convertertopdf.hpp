591ff4e8c878693e41b4d5002e685021d9c2707e
Константин Стуков <havendv@gmail.com>
2016-04-01 17:53:33 +1000

    Added simple convert(This is not multipaged)

--- qtreportslib/converters/convertertopdf.hpp
+++ qtreportslib/converters/convertertopdf.hpp
@@ -7,21 +7,52 @@
 namespace qtreports {
     namespace detail {
 
+        /*! @~russian
+        @brief Класс, используемый для конвертации документа в PDF формат.
+        @note Все функции данного класса, возвращающие bool,
+        возвращают true в случае успеха либо false, если во время
+        выполнения произошли ошибки. При этом с помощью метода getLastError()
+        можно получить описание последней произошедшей ошибки.
+
+        Класс, используемый для конвертации документа в PDF формат.
+        Поддерживает установку DPI.
+        */
         class ConverterToPDF {
 
         public:
             ConverterToPDF( const ReportPtr & report );
             ~ConverterToPDF();
 
+            /*! @~russian
+            Запускает процесс конвертации.
+            Путь может быть полным или относительным, примеры: 
+            "C:/example.pdf" "example.pdf"
+            @param[in] path Путь, по которому будет создан PDF документ.
+            */
             bool            convert( const QString & path );
 
+            /*! @~russian
+            Устанавливает DPI
+            @param[in] dpi Новое значение DPI
+            */
+            void            setDPI( int dpi );
+
+            /*! @~russian
+            Возвращает текущее значение DPI
+            */
+            int             getDPI() const;
+
+            /*! @~russian
+            Возвращает строку, содержащую описание последней произошедшей ошибки.
+            */
             const QString   getLastError() const;
 
         private:
             ReportPtr   m_report;
             QString     m_lastError;
+            int         m_dpi;
 
-            bool        createPDF( const QString & path );
+            bool        create( const QString & path );
 
         };
 
