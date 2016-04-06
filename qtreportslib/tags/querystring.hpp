2d38bc1731dea992722435f1cfc1aeccbae6f6b4
3LIZIUM <triper-s@mail.ru>
2016-04-06 19:47:47 +1000

    Update querystring.hpp

--- qtreportslib/tags/querystring.hpp
+++ qtreportslib/tags/querystring.hpp
@@ -7,13 +7,25 @@
 namespace qtreports {
     namespace detail {
 
+
+        /*! @~russian
+        @brief Класс, реализующий тэг <QueryString>
+        Класс, реализующий тэг <QueryString>
+        */
         class QueryString : public Object {
               
         public:
             QueryString();
             ~QueryString();
 
+            /*! @~russian
+            Задать Text
+            @param[in] text значение
+            */
             void            setText( const QString & text );
+            /*! @~russian
+            Получить Text
+            */            
             const QString   getText() const;
 
         private:
