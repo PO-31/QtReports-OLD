8b1cd732702d55d98b7e9236d6b400d8d4943fa4
3LIZIUM <triper-s@mail.ru>
2016-04-08 13:37:15 +1000

    Комменты

--- qtreportslib/tags/object.hpp
+++ qtreportslib/tags/object.hpp
@@ -10,15 +10,30 @@ namespace qtreports {
     namespace detail {
         bool    isEquals( const QString & string1, const QString & string2 );
 
+        /*! @~russian
+        @brief Базовый интерфейс обьекта для любого тега
+        */
         /* Базовый интерфейс объекта для любого тэга. */
         class Object {
 
         public:
             Object();
+            /*! @~russian
+            Конструктор, создается обьект с именем
+            @param[in] имя обьекта
+            */
             Object( const QString & name );
             virtual ~Object();
 
+            /*! @~russian
+            Устанавливает имя
+            @param[in] имя
+            */
             void            setName( const QString & name );
+            /*! @~russian
+            Устанавливает имя тега
+            @param[in] имя тега
+            */            
             void            setTagName( const QString & name );
             //virtual bool    setParameter( const QString & name, const QVariant & value );
 
