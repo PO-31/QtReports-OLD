8f7f6ade01b299474b467947c1e9c697217f840e
travic <travic@travic.org>
2016-04-10 13:16:07 +0000

    Update doc from Travic CI

--- html/search/functions_1.js
+++ html/search/functions_1.js
@@ -1,4 +1,8 @@
 var searchData=
 [
-  ['convert',['convert',['../classqtreports_1_1detail_1_1_converter_to_p_d_f.html#af39953d8db1bdcbbcf59cc00b05747eb',1,'qtreports::detail::ConverterToPDF']]]
+  ['close',['close',['../classqtreports_1_1_engine.html#a2a4b818b959aa56fedc37ffbdfae47f6',1,'qtreports::Engine']]],
+  ['convert',['convert',['../classqtreports_1_1detail_1_1_converter_to_p_d_f.html#af39953d8db1bdcbbcf59cc00b05747eb',1,'qtreports::detail::ConverterToPDF']]],
+  ['createhtml',['createHTML',['../classqtreports_1_1_engine.html#a551b0db0510d572d56447c471daa0891',1,'qtreports::Engine']]],
+  ['createpdf',['createPDF',['../classqtreports_1_1_engine.html#a34b6ccdf31af3bd275d9fb95100c6bda',1,'qtreports::Engine']]],
+  ['createwidget',['createWidget',['../classqtreports_1_1_engine.html#a2a0fef0c95335eec5df4f61e4ffa7434',1,'qtreports::Engine']]]
 ];
--- html/search/functions_4.js
+++ html/search/functions_4.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['object',['Object',['../classqtreports_1_1detail_1_1_object.html#afcb591b3e6eb863351d731eacaf9d302',1,'qtreports::detail::Object']]]
+  ['object',['Object',['../classqtreports_1_1detail_1_1_object.html#afcb591b3e6eb863351d731eacaf9d302',1,'qtreports::detail::Object']]],
+  ['open',['open',['../classqtreports_1_1_engine.html#a12cebdcb817b3c9a0c730b9439b2ff85',1,'qtreports::Engine']]]
 ];
