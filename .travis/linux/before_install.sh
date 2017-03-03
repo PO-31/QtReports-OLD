d3e9a901d748429dffc654a522849d1c01a68c0c
LancerX0 <lancerx0@gmail.com>
2017-03-04 01:34:11 +1000

    Set coverage test only local
    Signed-off-by: LancerX0 <lancerx0@gmail.com>

--- .travis/linux/before_install.sh
+++ .travis/linux/before_install.sh
@@ -10,6 +10,6 @@ if [ "$BUILD_TYPE" = "coverage" ]; then
   #sudo pip install --user cpp-coveralls
   sudo apt-get install -y ruby1.9.1 rubygems1.9.1
   gem install lcoveralls
-#  sudo update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby1.9.1 100
-#  sudo update-alternatives --install /usr/bin/gem gem /usr/bin/gem1.9.1 100
+  sudo update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby1.9.1 100
+  sudo update-alternatives --install /usr/bin/gem gem /usr/bin/gem1.9.1 100
 fi
