4ad8babfdf3f209e6e9498e49e1e46999fd23632
LancerX0 <lancerx0@gmail.com>
2017-03-04 00:17:12 +1000

    Recovered before_install.sh for clang
    Signed-off-by: LancerX0 <lancerx0@gmail.com>

--- .travis/linux/clang/before_install.sh
+++ .travis/linux/clang/before_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo apt-add-repository --yes ppa:beineri/opt-qt551
+sudo apt-add-repository --yes ppa:beineri/opt-qt551-trusty
 sudo apt-get update -qq
 sudo apt-get -f install clang qt-latest
