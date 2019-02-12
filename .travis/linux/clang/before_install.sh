f9f0c94a3a4bef88acb6f4c2c66b09fb9f7d869a
Alexander Khristosenko <dr.claws.sakh@gmail.com>
2019-02-13 03:18:29 +1000

    changed to always-latest package of qt for clang

--- .travis/linux/clang/before_install.sh
+++ .travis/linux/clang/before_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sudo add-apt-repository --yes ppa:beineri/opt-qt-5.10.0-xenial
-sudo apt-get update -qq
-sudo apt-get -f install clang qt-latest
+#sudo add-apt-repository --yes ppa:beineri/opt-qt-5.10.0-xenial
+#sudo apt-get update -qq
+#sudo apt-get -f install clang qt-latest
+sudo apt-get -f install clang qt5-default qttools5-dev-tools
