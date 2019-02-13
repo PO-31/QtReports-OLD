13ae749087cdc35d90d50e01d67d6ca0cd4ac215
Alexander Khristosenko <dr.claws.sakh@gmail.com>
2019-02-13 13:45:33 +1000

    Updated to clang-7

--- .travis/linux/clang/before_install.sh
+++ .travis/linux/clang/before_install.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-#sudo add-apt-repository --yes ppa:beineri/opt-qt-5.10.0-xenial
-#sudo apt-get update -qq
-#sudo apt-get -f install clang qt-latest
-sudo apt-get -f install clang qt5-default qttools5-dev-tools
+sudo apt-add-repository --yes "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-7 main"
+sudo apt-get update -qq
+sudo apt-get -f install -y clang-7 qt5-default qttools5-dev-tools
+
+update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.8 100
+update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-7 1000
+update-alternatives --install /usr/bin/clang++ clang /usr/bin/clang-3.8 100
+update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.8 100
+update-alternatives --install /usr/bin/clang clang /usr/bin/clang-7 1000
+update-alternatives --config clang
+update-alternatives --config clang++
