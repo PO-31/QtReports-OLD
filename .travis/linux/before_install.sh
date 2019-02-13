0297d84247b9c3262c448943dfab20bc8f409ecb
Alexander Khristosenko <dr.claws.sakh@gmail.com>
2019-02-14 09:35:33 +1000

    Added package to install for coverage build
    qtreportslib_tests requires qsqlite plugin to work with database and libqt5sql5-sqlite package has it

--- .travis/linux/before_install.sh
+++ .travis/linux/before_install.sh
@@ -3,9 +3,9 @@
 packages_list='qt5-default qttools5-dev-tools'
 if [ "$BUILD_TYPE" = "doxygen" ]; then 
   packages_list+=' doxygen'
+elif [ "$BUILD_TYPE" = "coverage" ]; then
+  packages_list+=' libqt5sql5-sqlite'
 fi
-
-echo $packages_list
 
 sudo apt-get update
 sudo apt-get install -y $packages_list
