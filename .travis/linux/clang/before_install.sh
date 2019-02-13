664fb5e095ef885a8da978f355de0d4e71623f06
Alexander Khristosenko <dr.claws.sakh@gmail.com>
2019-02-13 13:53:00 +1000

    Removed unnecessary clang package install

--- .travis/linux/clang/before_install.sh
+++ .travis/linux/clang/before_install.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-sudo apt-add-repository --yes "deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-7 main"
-sudo apt-get update -qq
-sudo apt-get -f install -y clang-7 qt5-default qttools5-dev-tools
-
-update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.8 100
-update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-7 1000
-update-alternatives --install /usr/bin/clang++ clang /usr/bin/clang-3.8 100
-update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.8 100
-update-alternatives --install /usr/bin/clang clang /usr/bin/clang-7 1000
-update-alternatives --config clang
-update-alternatives --config clang++
+sudo apt-get -f install -y qt5-default qttools5-dev-tools
